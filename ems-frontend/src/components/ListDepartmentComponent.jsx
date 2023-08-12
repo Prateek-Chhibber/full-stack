cc83ea79030c42b458a0e737851c7d12f590a9b0
prateekchhibber <0392pc@gmail.com>
2023-08-12 16:32:35 +0530

    Implement Delete Department Feature in React App

--- ems-frontend/src/components/ListDepartmentComponent.jsx
+++ ems-frontend/src/components/ListDepartmentComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {getAllDepartments} from './services/DepartmentService'
+import { deleteDepartment,getAllDepartments } from './services/DepartmentService'
 import { Link, useNavigate } from "react-router-dom";
 
 const ListDepartmentComponent = () => {
@@ -9,13 +9,17 @@ const ListDepartmentComponent = () => {
   const navigator = useNavigate();
 
   useEffect( () => {
+    listOfDepartments();
+  }, [])
+
+  function listOfDepartments(){
     getAllDepartments().then((response) => {
       // console.log(response.data);
       setDepartments(response.data);
     }).catch(error => {
       console.log(error);
     })
-  }, [])
+  }
 
   function updateDepartment(id){
     navigator(`/edit-department/${id}`)
