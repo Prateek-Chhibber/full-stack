7e2b4f92c236d9e43843320217138c7fdcaf8657
prateekchhibber <0392pc@gmail.com>
2023-08-12 15:51:36 +0530

    Add Department Form Handling

--- ems-frontend/src/components/DepartmentComponent.jsx
+++ ems-frontend/src/components/DepartmentComponent.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
 
 const DepartmentComponent = () => {
-  return (
-    <div>DepartmentComponent</div>
-  )
-}
+  const [departmentName, setDepartmentName] = useState("");
+  const [departmentDescription, setDepartmentDescription] = useState("");
 
-export default DepartmentComponent
+  const { id } = useParams();
+
+  const navigator = useNavigate();
+
+  function saveDepartment(e){
+    e.preventDefault();
+    const department = {departmentName, departmentDescription};
+    console.log(department);
+  }
+
+  function pageTitle(){
+    if(id){
+        return <h2 className='text-center'>Update Department</h2>
+    } else {
+        return <h2 className='text-center'>Add Department</h2>
+    }
+  }
+
+  return(
+    <div className='container'><br /><br />
+      <div className='row'>
+          <div className='card col-md-6 offset-md-3 offset-md-3'>
+              {
+                pageTitle()
+              }
+
+              <div className='card-body'>
+                  <form>
+                      <div className='form-group mb-2'>
+                          <label className='form-label'>Department Name:</label>
+                          <input
+                            type='text'
+                            name='departmentName'
+                            placeholder='Enter Department Name'
+                            className='form-control'
+                            value={departmentName}
+                            onChange={(e) => setDepartmentName(e.target.value)}
+
+                          >
+                          </input>
+                      </div>
+
+                      <div className='form-group mb-2'>
+                          <label className='form-label'>Department Description:</label>
+                          <input
+                            type='text'
+                            name='departmentDescription'
+                            placeholder='Enter Department Description'
+                            value={departmentDescription}
+                            onChange={(e) => setDepartmentDescription(e.target.value)}
+                            className='form-control'
+                          >
+                          </input>
+                      </div>
+                      <button className='btn btn-success mb-2' onClick={(e) => saveDepartment(e)}>Submit</button>
+                  </form>
+
+              </div>
+          </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default DepartmentComponent;
